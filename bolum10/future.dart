7d202c1511b82b04ba54f6db846805159b749490
kadersuutlu <sutlukader@gmail.com>
2023-05-30 15:05:35 +0300

    sixth commit

---
+++ bolum10/future.dart
@@ -0,0 +1,31 @@
+main(List<String> args) async {
+  print("Program basladı");
+  Future.delayed(Duration(seconds: 0), () {
+    print("0 saniyelik işlem");
+  });
+  print("Program bitti");
+
+  Future<int> toplam = Future(() {
+    int toplam = 0;
+    for (int i = 0; i < 1000000000; i++) {
+      toplam = toplam + i;
+    }
+    //return toplam;
+    throw Exception("Toplam hesaplanamadı");
+  });
+
+  //toplam.then((int toplam) => print(toplam)).catchError((hata) => print(hata));
+
+  try {
+    int forSonuc = await toplam;
+    print("**** $forSonuc");
+  } catch (e) {
+    print(e);
+  }
+
+// Future.value('emre) => MOCK DATA : gerçek verilerin simülasyonu
+  var f2 = Future.value('emre');
+  var f3 = Future.error('Hata ile biten future');
+
+  f3.catchError((hata) => print(hata));
+}
